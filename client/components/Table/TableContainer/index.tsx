8bc6b6d46559c491370113e620877e64526a17f6
Guilherme Mota <guilhermebromonschenkel@gmail.com>
2023-02-21 19:23:11 -0300

    feat(table): make component fully rounded

--- client/components/Table/TableContainer/index.tsx
+++ client/components/Table/TableContainer/index.tsx
@@ -4,7 +4,7 @@ const TableContainer: FC = (props) => {
 	const { children } = props
 
 	return (
-		<div className="relative overflow-x-auto sm:rounded-lg">
+		<div className="relative overflow-x-auto rounded-lg">
 			<table className="w-full text-sm text-left text-gray-500">
 				{children}
 			</table>
